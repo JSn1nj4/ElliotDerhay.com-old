17dc4792845779a6d1ae495f09d9f09c730b50c5
Elliot Derhay <e.j.derhay@gmail.com>
2017-03-09 12:52:13 -0500

    Explicitely import `projects` constant from API file

--- client/js/pages/ProjectsManager.js
+++ client/js/pages/ProjectsManager.js
@@ -1,4 +1,5 @@
 import '/imports/api/projectsmanager.js';
+import { projects } from '/imports/api/projects';
 Template.ManagerContent.onCreated(function managerOnCreated() {
   Meteor.subscribe('projects');
 });
