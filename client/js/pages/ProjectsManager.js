361c4d65f23b94897095c1b68ee774aaf733b671
Elliot Derhay <e.j.derhay@gmail.com>
2017-03-13 13:27:17 -0400

    Add first ManagerContent helper for working with projects data

--- client/js/pages/ProjectsManager.js
+++ client/js/pages/ProjectsManager.js
@@ -1,5 +1,12 @@
 import '/imports/api/projectsmanager.js';
 import { projects } from '/imports/api/projects';
+
 Template.ManagerContent.onCreated(function managerOnCreated() {
   Meteor.subscribe('projects');
+});
+
+Template.ManagerContent.helpers({
+  getProjectsList() {
+    return projects.find({}, { sort: { createdAt: -1} } ).fetch();
+  }
 });
