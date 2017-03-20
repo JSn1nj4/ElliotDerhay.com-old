4e73603c55770bf436a9babbfd87b6b52ac91da8
Elliot Derhay <e.j.derhay@gmail.com>
2017-03-20 19:24:49 -0400

    Add basic logout button with its functioning click event handler

--- client/js/pages/ProjectsManager.js
+++ client/js/pages/ProjectsManager.js
@@ -2,3 +2,9 @@ import '/imports/api/projectsmanager.js';
 Template.ManagerContent.onCreated(function managerOnCreated() {
   Meteor.subscribe('projects');
 });
+
+Template.ManagerContent.events({
+  'click #logout': (event) => {
+    Meteor.logout();
+  }
+});
