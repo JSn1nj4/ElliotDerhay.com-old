745ddf94e9adf8adeb45cfea5e339f5da9b7770d
Elliot Derhay <e.j.derhay@gmail.com>
2017-04-24 12:49:28 -0400

    Return validation value from SimpleSchema

--- server/imports/methods.js
+++ server/imports/methods.js
@@ -2,11 +2,13 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 Meteor.methods({
   adminLogin({ username, password }) {
-    new SimpleSchema({
+    let loginIsValid = new SimpleSchema({
       username: { type: String },
       password: { type: String }
     }).validate({ username, password });
 
-    return {testMsg: 'Login params validated'};
+    return {
+      loginIsValid: loginIsValid
+    };
   }
 });
