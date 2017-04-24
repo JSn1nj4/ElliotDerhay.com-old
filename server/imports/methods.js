6ac5886c8e8e75d94f6f659c2c8958d2ebee56e6
Elliot Derhay <e.j.derhay@gmail.com>
2017-04-24 12:05:56 -0400

    Enter the SimpleSchema package
    - Add `aldeed:simple-schema` 1.5.3 and deps
      - `mdg:validation-error` 0.2.0
    - Setup basic form field validation server-side
    - Update returned message to reflect what was tested

--- server/imports/methods.js
+++ server/imports/methods.js
@@ -1,5 +1,12 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
 Meteor.methods({
-  adminLogin({username, password}) {
-    return {testMsg: 'login checked'};
+  adminLogin({ username, password }) {
+    new SimpleSchema({
+      username: { type: String },
+      password: { type: String }
+    }).validate({ username, password });
+
+    return {testMsg: 'Login params validated'};
   }
 });
