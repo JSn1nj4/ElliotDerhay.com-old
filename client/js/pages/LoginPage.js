1013b780d53e17350a264fe22e36c8f0034bddda
Elliot Derhay <e.j.derhay@gmail.com>
2017-04-26 12:25:18 -0400

    Comments + initial check for field lengths
    The script will exit with an error message if either field is empty.

--- client/js/pages/LoginPage.js
+++ client/js/pages/LoginPage.js
@@ -1,8 +1,22 @@
 Template.LoginPage.events({
   'submit #login_form': (e) => {
+
+    /* Data prep
+        1. Prevent default form submit action
+        2. Grab username and password string values
+        3. Check that values aren't empty
+        4. Reset the form if value check passes */
     e.preventDefault();
     let username = String(e.target.username.value);
     let password = String(e.target.password.value);
+    if( username.length === 0 || password.length === 0) {
+      /* @TODO: set a reactive var that can be updated from here
+          Reason: This will replace the following `console.log()`, and allow setting up
+          a couple of helpers that can 1) check if the message is empty and 2) display whatever
+          error message is set */
+      console.log('Login fields not filled in');
+      return false; // Exit error handler if either login field is blank
+    }
     e.target.reset();
 
     Meteor.call('adminLogin', {username, password}, (err, result)=>{
