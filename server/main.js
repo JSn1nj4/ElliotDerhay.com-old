b941b54f508055dec353551eabac8296fb46290c
Elliot Derhay <e.j.derhay@gmail.com>
2017-07-25 13:06:43 -0400

    Add this temporary code to be removed later

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
+//import { Accounts } from 'meteor/accounts-base';
 import { projects } from '/imports/api/projects.js';
 import './imports/methods.js';
+
+// @TODO: Remove the following startup code before first release
+// Meteor.startup(function() {
+//   Accounts.setPassword( Meteor.users.find({username: 'JSn1nj4'}).fetch()[0]._id, '@FCV!x5z$%6HFcmSO%TTApWP*xQiB19@');
+// });
 
 Meteor.publish('projects', function projectsPublication() {
   return projects.find({});
