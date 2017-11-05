0e0e3788d264d5417e4db48fa83dece44790cdd7
Elliot Derhay <e.j.derhay@gmail.com>
2017-11-05 16:17:49 -0500

    In fixtures, make sure my admin user account exists
    Will probably handle this differently for the live site, but this works
    for now.
    
    Also, I deleted some unnecessary commented lines of code from the
    server's startup 'index.js' file.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,10 +1,4 @@
-//import { Accounts } from 'meteor/accounts-base';
+// Server startup entry point
 import '/imports/api/projects/methods.js';
 import '/imports/api/projects/server/publications.js';
 import './fixtures.js';
-// import './imports/methods.js';
-
-// @TODO: Remove the following startup code before first release
-// Meteor.startup(function() {
-//   Accounts.setPassword( Meteor.users.find({username: 'JSn1nj4'}).fetch()[0]._id, '@FCV!x5z$%6HFcmSO%TTApWP*xQiB19@');
-// });
