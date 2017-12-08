325dfda9f12b698938f9df32cec3fcb86fd0fd31
Elliot Derhay <e.j.derhay@gmail.com>
2017-12-08 17:55:23 -0500

    Finalize route setup with dynamic imports
    Unfortunately, this means having to wipe out my custom routing
    function... :cry:
    
    Fortunately, creating separate routes may help me later if I need to
    work with different pieces of data for each route. I don't foresee this
    for this particular project, but it's probably good to be in the habit
    of defining routes separately -- even though most of them share common
    properties.
    
    I've also learned how to import specific modules for all routes and how
    to setup router groups, which are both positives. All good in the end.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,40 +1,76 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-// header and footer components; these need to be imported before the layouts
-import '/imports/ui/components/Header.js';
-import '/imports/ui/components/Footer.js';
+// Public routes
+FlowRouter.globals.push({
+  waitOn() {
+    return [
+      import('/imports/ui/components/Header.js'),
+      import('/imports/ui/components/Footer.js'),
+      import('/imports/ui/pages/ComingSoon.js')
+    ];
+  }
+});
 
-// layouts for use in routes
-import '/imports/ui/layouts/PageLayout.js';
-import '/imports/ui/layouts/ManagerLayout.js';
+let publicRoutes = FlowRouter.group({
+  name: 'public',
+  waitOn() {
+    return [
+      import('/imports/ui/layouts/PageLayout.js')
+    ];
+  }
+});
 
-// pages rendered by the route
-import '/imports/ui/pages/ComingSoon.js';
-import '/imports/ui/pages/Home.js';
-import '/imports/ui/pages/Projects.js';
-import '/imports/ui/pages/Contact.js';
+publicRoutes.route('/', {
+  name: 'Home',
+  action() {
+    import('/imports/ui/pages/Home.js').then(() => {
+      BlazeLayout.render('PageLayout', {
+        content: 'Home'
+      });
+    });
+  }
+});
 
-// Client-side routes
-let routes = [
-  {route: '/', name: 'Home', template: 'PageLayout', content: 'Home'},
-  {route: '/projects', name: 'Projects', template: 'PageLayout', content: 'Projects'},
-  {route: '/projects/manager', name: 'Projects Manager', template: 'ManagerLayout', content: ''},
-  {route: '/experiments', name: 'Experiments', template: 'PageLayout', content: 'ComingSoon'},
-  {route: '/contact', name: 'Contact', template: 'PageLayout', content: 'ComingSoon'},
-];
+publicRoutes.route('/projects', {
+  name: 'Projects',
+  action() {
+    import('/imports/ui/pages/Projects.js').then(() => {
+      BlazeLayout.render('PageLayout', {
+        content: 'Projects'
+      });
+    });
+  }
+});
 
-routes.map(r => {
-  FlowRouter.route(r.route, {
-    name: r.name,
-    action() {
-      if(r.content && r.content.length >= 1) {
-        BlazeLayout.render(r.template, {
-          content: r.content
-        });
-      } else {
-        BlazeLayout.render(r.template);
-      }
-    }
-  });
+publicRoutes.route('/experiments', {
+  name: 'Experiments',
+  action() {
+    import('/imports/ui/pages/ComingSoon.js').then(() => {
+      BlazeLayout.render('PageLayout', {
+        content: 'ComingSoon'
+      });
+    });
+  }
+});
+
+publicRoutes.route('/contact', {
+  name: 'Contact',
+  action() {
+    import('/imports/ui/pages/ComingSoon.js').then(() => {
+      BlazeLayout.render('PageLayout', {
+        content: 'ComingSoon'
+      });
+    });
+  }
+});
+
+// Private routes
+FlowRouter.route('/projects/manager', {
+  name: 'ProjectsManager',
+  action() {
+    import('/imports/ui/layouts/ManagerLayout.js').then(() => {
+      BlazeLayout.render('ManagerLayout');
+    });
+  }
 });
