be79d081fd7801751b0fec1c3d7829396f477562
Elliot Derhay <e.j.derhay@gmail.com>
2017-12-12 09:13:43 -0500

    Add and test email package for first time

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,3 +2,13 @@
 import '/imports/api/projects/methods.js';
 import '/imports/api/projects/server/publications.js';
 import './fixtures.js';
+
+import { Email } from 'meteor/email';
+
+Email.send({
+  from: 'elliot@example.com',
+  to: 'recipient@example.com',
+  replyTo: 'elliot@example.com',
+  text: `Test email.
+  Random number: ${Math.random()}`
+});
