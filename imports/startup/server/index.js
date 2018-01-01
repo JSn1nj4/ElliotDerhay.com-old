1c69d622e1be0313e050aa0c76e2fab9953c0e70
Elliot Derhay <e.j.derhay@gmail.com>
2018-01-01 10:03:59 -0500

    Don't try to use SSL just yet
    Will figure out later.
    
    Refers to issue #29.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,17 +1,18 @@
 // Server startup entry point
-import { SSLProxy } from 'meteor/tarang:ssl';
+// @TODO: Figure out how to use SSL later. Would be nice to have.
+// import { SSLProxy } from 'meteor/tarang:ssl';
 import '/imports/api/projects/methods.js';
 import '/imports/api/projects/server/publications.js';
 import './fixtures.js';
 
 if(process.env.NODE_ENV == 'development') {
   process.env.ROOT_URL = 'elliotderhay.test';
-  SSLProxy({
-    port: 6000,
-    ssl: {
-      key: Assets.getText('certs/ed-key.pem'),
-      cert: Assets.getText('certs/ed-cert.pem'),
-      ca: Assets.getText('certs/ed-csr.pem')
-    }
-  });
+  // SSLProxy({
+  //   port: 6000,
+  //   ssl: {
+  //     key: Assets.getText('certs/ed-key.pem'),
+  //     cert: Assets.getText('certs/ed-cert.pem'),
+  //     ca: Assets.getText('certs/ed-csr.pem')
+  //   }
+  // });
 }
