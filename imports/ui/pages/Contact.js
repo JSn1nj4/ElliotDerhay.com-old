26e35bb7c0d798438d5be819927783161d49ba93
JSn1nj4 <e.j.derhay@gmail.com>
2018-02-04 10:17:05 -0500

    Import/use HeroImage as first layout element

--- imports/ui/pages/Contact.js
+++ imports/ui/pages/Contact.js
@@ -1,1 +1,20 @@
+import '../components/HeroImage.js';
 import './Contact.html';
+
+Template.Contact.onCreated(function contactOnCreated() {
+  this.heroSettings = {
+    id: 'contact_banner',
+    content: {
+      title: 'GET IN TOUCH',
+      tagline: 'Lorem ipsum dolor sit amet, mea no movet scaevola assueverit, pro ferri choro maiestatis in'
+    },
+    layout: '12,4',
+    contentColumn: 1
+  };
+});
+
+Template.Contact.helpers({
+  getHeroSettings() {
+    return Template.instance().heroSettings;
+  }
+});
