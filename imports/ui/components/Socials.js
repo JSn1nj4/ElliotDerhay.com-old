1ba350551fca4e357369d9116db6e3f11b809bfc
JSn1nj4 <e.j.derhay@gmail.com>
2018-02-09 08:11:45 -0500

    Add social icons, start building Socials component

--- imports/ui/components/Socials.js
+++ imports/ui/components/Socials.js
@@ -1,1 +1,14 @@
 import './Socials.html';
+
+Template.Socials.onCreated(function socialsOnCreated(){
+  // console.log(Meteor.call(''));
+});
+
+Template.Socials.helpers({
+  getGitHubIcon() {
+
+  },
+  getTwitterIcon() {
+
+  }
+});
