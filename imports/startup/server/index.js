0a1652eb448363e04e6d4e7d182459f360697589
JSn1nj4 <e.j.derhay@gmail.com>
2018-02-19 20:17:15 -0500

    Guess it's important to import the method server-side

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -5,6 +5,7 @@ import '/imports/api/projects/methods.js';
 import '/imports/api/projects/server/publications.js';
 import '/imports/api/socials/server/publications.js';
 import '/imports/api/socials/methods.js';
+import '/imports/api/email/methods.js';
 import './fixtures.js';
 
 if(process.env.NODE_ENV == 'development') {
