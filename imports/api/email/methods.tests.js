7efe9559cde8be4515b13b8780e2bddd15613287
Elliot Derhay <e.j.derhay@gmail.com>
2018-02-27 12:25:58 -0500

    Divide tests by whether they require direct interactions

--- imports/api/email/methods.tests.js
+++ imports/api/email/methods.tests.js
@@ -4,27 +4,31 @@ import './methods.js';
 
 if(Meteor.isServer) {
   describe('Email sending methods', function() { // eslint-disable-line no-undef
-    it('can send contact form emails', function() { // eslint-disable-line no-undef
-      const sendContactEmail = Meteor.server.method_handlers['email.send.contact'];
-      const formData = {
-        firstname: 'firstname',
-        lastname: 'lastname',
-        email: 'test@example.com',
-        subject: 'Test subject',
-        message: 'test message'
-      };
+    describe('for client-side interactions', function() { // eslint-disable-line no-undef
+      it('can send contact form emails', function() { // eslint-disable-line no-undef
+        const sendContactEmail = Meteor.server.method_handlers['email.send.contact'];
+        const formData = {
+          firstname: 'firstname',
+          lastname: 'lastname',
+          email: 'test@example.com',
+          subject: 'Test subject',
+          message: 'test message'
+        };
 
-      const result = sendContactEmail.call({}, { formData });
+        const result = sendContactEmail.call({}, { formData });
 
-      assert.equal(result, 'Email sent!');
+        assert.equal(result, 'Email sent!');
+      });
+
+      it('can send password-reset emails', function() { // eslint-disable-line no-undef
+
+      });
     });
 
-    it('can send password-reset emails', function() { // eslint-disable-line no-undef
+    describe('for automatic notifications', function() { // eslint-disable-line no-undef
+      it('can send login notification emails', function() { // eslint-disable-line no-undef
 
-    });
-
-    it('can send login notification emails', function() { // eslint-disable-line no-undef
-
+      });
     });
   });
 }
