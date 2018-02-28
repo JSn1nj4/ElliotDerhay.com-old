9969571c0557d450ddbc5edc48ec16e27f182461
Elliot Derhay <e.j.derhay@gmail.com>
2018-02-28 13:04:01 -0500

    Create EmailSender test groups

--- imports/api/email/EmailSender.tests.js
+++ imports/api/email/EmailSender.tests.js
@@ -1,1 +1,15 @@
 import { EmailSender } from './EmailSender.js';
+import { assert } from 'chai';
+import { Meteor } from 'meteor/meteor';
+
+if(Meteor.isServer) {
+  describe('Expected EmailSender class behavior', function() { // eslint-disable-line no-undef
+    describe('Expect email sending to succeed', function() { // eslint-disable-line no-undef
+
+    });
+
+    describe('Expect email sending to fail', function() { // eslint-disable-line no-undef
+
+    });
+  });
+}
