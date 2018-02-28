7909deb1205eb62e7d4196078ef86d87b0d05e8d
Elliot Derhay <e.j.derhay@gmail.com>
2018-02-28 13:13:32 -0500

    All tests will fail initially

--- imports/api/email/EmailSender.tests.js
+++ imports/api/email/EmailSender.tests.js
@@ -4,12 +4,34 @@ import { Meteor } from 'meteor/meteor';
 
 if(Meteor.isServer) {
   describe('Expected EmailSender class behavior', function() { // eslint-disable-line no-undef
-    describe('Expect email sending to succeed', function() { // eslint-disable-line no-undef
+    describe('Email object instantiation', function() { // eslint-disable-line no-undef
+      it('create email object in test mode', function() { // eslint-disable-line no-undef
+        return false;
+      });
 
+      it('create email object in live mode', function() { // eslint-disable-line no-undef
+        return false;
+      });
+
+      it('fail to create email object when setting both params', function() { // eslint-disable-line no-undef
+        return false;
+      });
+    });
+
+    describe('Expect email sending to succeed', function() { // eslint-disable-line no-undef
+      it('email sends in test mode', function() { // eslint-disable-line no-undef
+        return false;
+      });
+
+      it('email sends in live mode', function() { // eslint-disable-line no-undef
+        return false;
+      });
     });
 
     describe('Expect email sending to fail', function() { // eslint-disable-line no-undef
-
+      it('email validation fails when formatted incorrectly', function() { // eslint-disable-line no-undef
+        return false;
+      });
     });
   });
 }
