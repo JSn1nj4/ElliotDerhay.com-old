93684d5484c838f631186195e8ae752f1214bad7
Elliot Derhay <e.j.derhay@gmail.com>
2018-03-30 22:54:36 -0400

    Beginning stages of mobile menu

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -1,9 +1,24 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { ReactiveVar } from 'meteor/reactive-var';
 import './Header.html';
+
+Template.Header.onCreated(function headerOnCreated() {
+  this.openMobileMenu = new ReactiveVar(false);
+});
+
+Template.Header.events({
+  'change input[name="mobile_menu_toggle"]'(e, tpl) {
+    tpl.openMobileMenu.set(tpl.openMobileMenu.get() ? false : true);
+    console.log(tpl.openMobileMenu.get());
+  }
+});
 
 Template.Header.helpers({
   route: function() {
     FlowRouter.watchPathChange();
     return FlowRouter.current().path;
+  },
+  toggleState() {
+    return Template.instance().openMobileMenu.get() ? 'open' : 'closed';
   }
 });
